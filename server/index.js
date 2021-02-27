03b79d90623ffda1d542175d13d76cc1dce36b03
Nikita Ermilov <ermilov1999@gmail.com>
2021-02-27 13:56:05 +0300

    Express server done. ESlint added via pre-commit hook.

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('src'));
+
+app.all('*', (req, res) => res.sendFile(`${__dirname}/src/index.html`));
+
+app.listen(port);
