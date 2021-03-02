ff1814712fa09cf1cfdeb846e8f53d8596d36721
Nikita Ermilov <ermilov1999@gmail.com>
2021-03-02 18:09:13 +0300

    added pm2

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const port = 80;
 
 app.use(express.static('src'));
 
-app.all('*', (req, res) => res.sendFile(`${__dirname}/src/index.html`));
+app.all('*', (req, res) => res.sendFile(`${__dirname}/../src/index.html`));
 
 app.listen(port);
