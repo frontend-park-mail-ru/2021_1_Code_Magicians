f10ce738e1e266bcd2c46cbe71953730680a5e96
Nikita Ermilov <ermilov1999@gmail.com>
2021-03-15 17:43:36 +0300

    removed layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import {PinBuilderView} from './views/pinBuilderView/pinBuilderView.js';
-
-const props = {
-  user: {
-    avatarURL: 'assets/img/default-avatar.png',
-    isAuthorized: false,
-  },
-};
-
-const application = document.getElementById('app');
-
-const view = new PinBuilderView(props, application);
-
-view.show();
+// const application = document.getElementById('app');
