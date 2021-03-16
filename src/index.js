9d96db3edffbfdcc3327aec64f749e26bb93e756
Nikita Ermilov <ermilov1999@gmail.com>
2021-03-16 17:12:34 +0300

    first version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// const application = document.getElementById('app');
+import {Sidebar} from './components/sidebar/sidebar.js';
+
+const application = document.getElementById('app');
+
+const sidebar = new Sidebar({});
+
+application.innerHTML = sidebar.render();
