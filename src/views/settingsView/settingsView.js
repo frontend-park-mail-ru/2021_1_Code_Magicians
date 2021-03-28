6f9b7f54be53daaeda051c5904395f7f77e5e073
Nikita Ermilov <ermilov1999@gmail.com>
2021-03-29 00:53:52 +0300

    password settings done

--- src/views/settingsView/settingsView.js
+++ src/views/settingsView/settingsView.js
@@ -1,6 +1,8 @@
 import {View} from '../view.js';
 import {Page} from '../../components/page/page.js';
+// eslint-disable-next-line no-unused-vars
 import {ProfileChanges} from '../../components/profileChanges/profileChanges.js';
+import {SecuritySettings} from '../../components/securitySettings/securitySettings.js';
 
 /**
  * Profile settings view
@@ -24,7 +26,8 @@ export class SettingsView extends View {
       ...this.props,
       page__content: tmpl({
         ...this.props,
-        settingsForm: new ProfileChanges(this.props).render(),
+        settingsForm: new SecuritySettings(this.props).render(),
+        // settingsForm: new ProfileChanges(this.props).render(),
       }),
     });
 
