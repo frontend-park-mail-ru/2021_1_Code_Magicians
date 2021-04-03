da03e3a4a1971511e09b7b6ab0788feb92a193fe
Nikita Ermilov <ermilov1999@gmail.com>
2021-04-03 16:19:28 +0300

    small fixes done. server path args and scss fixed.

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = 8081;
+const port = process.argv[2] || 3000;
 
 app.use(express.static('src'));
 
 app.all('*', (req, res) => {
-  console.log(req.path);
   res.sendFile(path.resolve('src/index.html'));
 });
 
