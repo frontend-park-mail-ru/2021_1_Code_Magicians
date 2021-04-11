bf58afc130d9c9ac48d62b9ae90a75b777cfead2
Nikita Ermilov <ermilov1999@gmail.com>
2021-04-11 15:14:38 +0300

    fixed regexp

--- src/consts/regexp.js
+++ src/consts/regexp.js
@@ -1,7 +1,7 @@
 export const pathParamRegExp = /(:[^\/]+)(?:\/|$)/g;
 export const regSubstr = '[^\\/]+';
 export const urlRegexp = /https*:\/\/[-._a-zA-Z0-9:]+/;
-export const emailRegexp = /^\S+@\S+\.\S+$/;
+export const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const usernameRegexp = /^[a-zA-Z0-9_]{2,42}$/;
-export const passwordRegexp = /^\S.{8,30}$/;
+export const passwordRegexp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 export const firstNameRegexp = /[a-zA-Z ]{0,42}/;
