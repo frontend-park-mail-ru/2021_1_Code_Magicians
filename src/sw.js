d3542d4bbe90cfd42744eea547b70bc6b64da523
Nikita Ermilov <ermilov1999@gmail.com>
2021-05-27 22:55:06 +0300

    api remake

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-invalid-this */
 // noinspection ThisExpressionReferencesGlobalObjectJS
 
-const backendURL = 'https://pinter-best.com';
+const backendURL = 'https://pinter-best.com/api';
 
 const CACHE_NAME = 'pinterbest-main-cache';
 const cacheURLs = [
