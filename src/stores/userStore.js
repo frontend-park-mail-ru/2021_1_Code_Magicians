3dff962cf5e87c1078d6c28e8acec7aac33a28c4
Nikita Ermilov <ermilov1999@gmail.com>
2021-05-28 22:18:19 +0300

    tmp

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -573,7 +573,7 @@ class UserStore extends Store {
    * @return {Boolean}
    */
   hasNewMessage() {
-    return this._newMessage;
+    return this._chats.some((chat) => !chat.isRead);
   }
 
   /**
